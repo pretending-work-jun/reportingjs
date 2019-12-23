ac56fac77e89ff82a67bb5fd6d40365557f17014
Jun <jiajun.koh@digi.com.my>
2019-12-23 17:54:33 +0800

    Refactor, housekeeping chores

--- src/typeParser.js
+++ src/typeParser.js
@@ -4,19 +4,12 @@
  * @return {any}
  */
 const JSONParse = (word) => {
-
-  if (typeof word != '')
   try {
     return JSON.parse(word);
   } catch (err) {
     return word;
   }
 };
-
-/**
- * doNothing()
- */
-const doNothing = () => {};
 
 export default {
   JSONParse,
